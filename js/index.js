5dd46f9d238d408ae1e57e79ade1ac18f8202bee
Arvin Agas <arvagas@gmail.com>
2019-06-17 16:07:46 -0400

    Initial commit added navlinks and images

--- js/index.js
+++ js/index.js
@@ -40,3 +40,17 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const navLinks = document.querySelectorAll('nav a')
+navLinks[0].textContent = siteContent['nav']['nav-item-1']
+navLinks[1].textContent = siteContent['nav']['nav-item-2']
+navLinks[2].textContent = siteContent['nav']['nav-item-3']
+navLinks[3].textContent = siteContent['nav']['nav-item-4']
+navLinks[4].textContent = siteContent['nav']['nav-item-5']
+navLinks[5].textContent = siteContent['nav']['nav-item-6']
+
+const ctaImg = document.getElementById('cta-img')
+ctaImg.setAttribute('src', siteContent['cta']['img-src'])
+
+const midImg = document.getElementById('middle-img')
+midImg.setAttribute('src',siteContent['main-content']['middle-img-src'])
