db94633ebd5c7b5c01c3763c5ec1819c3be967e1
Arvin Agas <arvagas@gmail.com>
2019-06-18 02:33:19 -0400

    Added Start/Reset buttons via DOM coded Start button to begin timer adjusted timer function code to be called

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,3 +1,17 @@
+// Buttons
+const start = document.createElement('button')
+start.innerText = 'Start'
+const reset = document.createElement('button')
+reset.innerText = 'Reset'
+document.querySelector('body').appendChild(start)
+document.querySelector('body').appendChild(reset)
+
+start.addEventListener('click', () => {
+    start.disabled=true
+    startTime = Date.now()
+    countUp = setInterval(timer, 1) // Updates every 1ms
+})
+
 // Selectors
 let secondTensDisp = document.querySelector('#secondTens')
 let secondOnesDisp = document.querySelector('#secondOnes')
@@ -6,19 +20,23 @@ let msTensDisp = document.querySelector('#msTens')
 const digits = document.querySelectorAll('.digits')
 
 // Initial Variables
-let count = 0
-let startTime = Date.now()
+let startTime
+let countUp
 
 // Timer counting until 10 seconds
-let timer = setInterval(() => {
+let timer = () => {
     let timeInterval = Date.now() - startTime
     msTensDisp.textContent = timeInterval % 10
     msHundredsDisp.textContent = Math.floor(timeInterval / 100) % 10
     secondOnesDisp.textContent = Math.floor(timeInterval / 1000) % 10
     secondTensDisp.textContent = Math.floor(timeInterval / 10000) % 10
     if (timeInterval >= 10000) {
-        clearInterval(timer)
+        clearInterval(countUp)
         msTensDisp.textContent = 0 // set msTens to 0 to account for update inaccuracies
         digits.forEach(item => item.classList.toggle('redDigit'))
     }
-}, 1)
+}
+
+const startTimer = () => {
+
+}
