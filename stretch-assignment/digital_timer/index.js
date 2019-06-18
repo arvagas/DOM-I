1d15f3d89824d5ce55a36a0dbfe7a5910fab9f90
Arvin Agas <arvagas@gmail.com>
2019-06-18 11:19:24 -0400

    Gave better reset button logic to prevent spam reset click

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,23 +1,37 @@
+const buttonBar = document.createElement('div')
+document.querySelector('body').appendChild(buttonBar)
+
 // Buttons
 const start = document.createElement('button')
 start.innerText = 'Start'
 const reset = document.createElement('button')
 reset.innerText = 'Reset'
-document.querySelector('body').appendChild(start)
-document.querySelector('body').appendChild(reset)
+buttonBar.appendChild(start)
+buttonBar.appendChild(reset)
+reset.disabled = true
 
 start.addEventListener('click', () => {
     start.disabled=true
+    reset.disabled=false
     startTime = Date.now()
     countUp = setInterval(timer, 1) // Updates every 1ms
 })
 reset.addEventListener('click', () => {
-    start.disabled=false
-    msTensDisp.textContent = '-'
-    msHundredsDisp.textContent = '-'
-    secondOnesDisp.textContent = '-'
-    secondTensDisp.textContent = '-'
-    digits.forEach(item => item.classList.toggle('redDigit'))
+    if (counter >= 10000) {
+        start.disabled=false
+        secondTensDisp.textContent = '-'
+        secondOnesDisp.textContent = '-'
+        msHundredsDisp.textContent = '-'
+        msTensDisp.textContent = '-'
+        digits.forEach(item => item.classList.toggle('redDigit'))
+        reset.disabled=true
+    }
+    else if (counter < 10000) {
+        clearInterval(countUp)
+        counter = 0
+        startTime = Date.now()
+        countUp = setInterval(timer, 1) // Updates every 1ms
+    }
 })
 
 // Selectors
@@ -30,21 +44,18 @@ const digits = document.querySelectorAll('.digits')
 // Initial Variables
 let startTime
 let countUp
+let counter
 
 // Timer counting until 10 seconds
 let timer = () => {
-    let timeInterval = Date.now() - startTime
-    msTensDisp.textContent = timeInterval % 10
-    msHundredsDisp.textContent = Math.floor(timeInterval / 100) % 10
-    secondOnesDisp.textContent = Math.floor(timeInterval / 1000) % 10
-    secondTensDisp.textContent = Math.floor(timeInterval / 10000) % 10
-    if (timeInterval >= 10000) {
+    counter = Date.now() - startTime
+    msTensDisp.textContent = counter % 10
+    msHundredsDisp.textContent = Math.floor(counter / 100) % 10
+    secondOnesDisp.textContent = Math.floor(counter / 1000) % 10
+    secondTensDisp.textContent = Math.floor(counter / 10000) % 10
+    if (counter >= 10000) {
         clearInterval(countUp)
         msTensDisp.textContent = 0 // set msTens to 0 to account for update inaccuracies
         digits.forEach(item => item.classList.toggle('redDigit'))
     }
-}
-
-const startTimer = () => {
-
 }
